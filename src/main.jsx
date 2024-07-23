081c7a4a81d8482578ff11906ad7265c3e514011
mayowasamuel <fpasamuelmayowa51@gmail.com>
2024-07-23 21:06:24 +0100

    HoneyBookWidget

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ParallaxProvider } from "react-scroll-parallax";
+import HoneyBookWidget from "./HoneyBook.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ParallaxProvider>
-      <HoneyBook>
+      <HoneyBookWidget>
         <App />
-      </HoneyBook>
+      </HoneyBookWidget>
     </ParallaxProvider>
   </React.StrictMode>
 );
