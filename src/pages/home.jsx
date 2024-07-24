788e122d900868507238ea58268f9424aae74af2
Timi <tunmisebabade@gmail.com>
2024-07-24 10:04:06 +0100

    first commit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
                 <p className="md:text-xl text-base font-light">Bams Visuals is widely considered one of the top 3 African wedding videographers in the world. We routinely travel documenting and crafting beautiful luxury films for clients around the United States. Our style is a fusion of cinematic flair, documentary grit, and a deep love for the human experience</p>
             </div>
             <div className='testimonial md:py-28 py-10'>
-                <div className='relative z-10 bg-[#F6E7BE] lg:w-[700px] w-[90%] mx-auto'>
+                <div className='relative z-10 bg-[#F6E7BE] lg:w-[700px] w-[90%] mx-auto cursor-grab'>
                     <Splide options={{
                         type: 'loop',
                         perPage: 1,
