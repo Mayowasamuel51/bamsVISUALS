6aa71e4c05b939569ad308b39d5d4189a50b733a
Timi <tunmisebabade@gmail.com>
2024-07-31 01:46:38 +0100

    first commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,9 +5,25 @@ import { FaXmark } from "react-icons/fa6";
 import { motion, AnimatePresence } from "framer-motion"
 import Mobilenav from "./mobilenav";
 import logo from "../assets/images/logo2.png";
+import logo2 from "../assets/images/logo-gold.png";
+
+
 
 const Navbar = () => {
     const [bg, handleBg] = useState(false)
+    const imgVariant = {
+        initial: {
+            opacity: 1,
+            rotateY: 0
+        },
+        animate: {
+            rotateY: bg ? 360 : 0,
+            transition: { duration: 0.5 }
+        },
+        exit: {
+            rotate: 90
+        }
+    }
     const [toggleNav, setToggleNav] = useState(false)
     const handleNav = () => {
         setToggleNav(prev => !prev)
@@ -26,9 +42,16 @@ const Navbar = () => {
         <header className={`z-20 fixed w-full top-0 right-0 left-0 flex justify-between items-center md:px-20 px-4 py-2 ${bg && "shadow-md"} duration-300`}>
             <div className={`${bg && "bg-white h-full"} duration-300 h-0 origin-top absolute inset-0 -z-10`}></div>
             <Link to="/">
-                <div className="">
-                    <img src={logo} className="w-20 md:w-28" alt="logo" />
-                </div>
+                <AnimatePresence>
+                    <motion.div
+                        variants={imgVariant}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                    >
+                        <img src={bg ? logo2 : logo} alt="logo" className="w-20 md:w-20 h-22 object-cover" />
+                    </motion.div>
+                </AnimatePresence>
             </Link>
             <div className="md:block hidden">
                 <nav>
