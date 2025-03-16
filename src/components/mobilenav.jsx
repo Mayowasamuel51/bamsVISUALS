1e52ca9240c2bc021da7899f751eabf924aa30cc
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-03-16 13:09:28 +0100

    send code

--- src/components/mobilenav.jsx
+++ src/components/mobilenav.jsx
@@ -1,25 +1,41 @@
-import { NavLink } from "react-router-dom"
-import { motion } from "framer-motion"
-import PropTypes from 'prop-types';
+import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
+import PropTypes from "prop-types";
 
-
-const Mobilenav = ({handleNav}) => {
-    return (
-        <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} className="min-h-screen fixed inset-0 bg-black bg-opacity-50 p-10 block md:hidden">
-            <motion.nav initial={{scale: 0}} animate={{scale: 1}} className="bg-white rounded-md py-20">
-                <ul className={`flex flex-col items-center gap-8`}>
-                    <li onClick={handleNav} className="hover:opacity-70 duration-300"><NavLink className={({ isActive }) => isActive && "text-text"} to="/">Home</NavLink></li>
-                    <li onClick={handleNav} className="hover:opacity-70 duration-300"><NavLink className={({ isActive }) => isActive && "text-text"} to="/gallery">Gallery</NavLink></li>
-                    <li onClick={handleNav} className="hover:opacity-70 duration-300"><NavLink className={({ isActive }) => isActive && "text-text"} to="contact">Contact</NavLink></li>
-                    <li onClick={handleNav} className="hover:opacity-70 duration-300"><NavLink className={({ isActive }) => isActive && "text-text"} to="/about">About</NavLink></li>
-                </ul>
-            </motion.nav>
-        </motion.div>
-    )
-}
+const Mobilenav = ({ handleNav }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center md:hidden"
+    >
+      <motion.nav
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        className="bg-white rounded-md py-20 px-10 w-3/4 shadow-lg"
+      >
+        <ul className="flex flex-col items-center gap-8 text-black font-semibold text-lg">
+          <li onClick={handleNav} className="hover:text-gray-500 duration-300">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li onClick={handleNav} className="hover:text-gray-500 duration-300">
+            <NavLink to="/gallery">Gallery</NavLink>
+          </li>
+          <li onClick={handleNav} className="hover:text-gray-500 duration-300">
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li onClick={handleNav} className="hover:text-gray-500 duration-300">
+            <NavLink to="/about">About</NavLink>
+          </li>
+        </ul>
+      </motion.nav>
+    </motion.div>
+  );
+};
 
 Mobilenav.propTypes = {
-    handleNav: PropTypes.func,
-}
+  handleNav: PropTypes.func,
+};
 
-export default Mobilenav
+export default Mobilenav;
