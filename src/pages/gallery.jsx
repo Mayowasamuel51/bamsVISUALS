2595faece0e4f0e59f16a119817964a14a7f46e2
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-03-20 11:34:12 +0100

    new enw

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -10,6 +10,7 @@ import ReactPlayer from "react-player";
 import { Link } from "react-router-dom";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import { motion } from "framer-motion";
 // import Mobilenav from "./mobilenav";
 import logo from "../assets/images/logo2.png";
 import logo2 from "../assets/images/logo-gold.png";
@@ -20,20 +21,20 @@ const videos = [
   // { url: " https://vimeo.com/1010058915/c7c983dfc5", light: gallery_six },
   { url: "https://vimeo.com/990177049/e349d31ec3", light: gallery_six },
 
-  {
-    url: "https://www.dropbox.com/scl/fi/te2r7y8c4j3vq457hvd71/teaser-2160p-1.mp4?rlkey=93s9yy5a7xghggjzfgyx113ub&st=zg4tmxi6&dl=0",
-    light: gallery_one,
-  },
-  {
-    url: "https://www.dropbox.com/scl/fi/al3ytsl8ag93mn0ntfz33/IMG_0520.PNG?rlkey=lmow2oxxyv7sp7wwad41ky4h6&st=tghnhjyw&dl=0",
-    light: gallery_two,
-  },
-  { url: "https://vimeo.com/990845187/f49e83fd83", light: gallery_five },
-  {
-    url: "https://www.dropbox.com/scl/fi/qot2n92kio5msjwank9v5/IMG_0521.jpg?rlkey=1c23u59gb5nz8eqenxazhnwkj&st=k6dob4u6&dl=0",
-    light: gallery_three,
-  },
-  { url: "https://vimeo.com/990179377/a9a9266311", light: gallery_four },
+  // {
+  //   url: "https://www.dropbox.com/scl/fi/te2r7y8c4j3vq457hvd71/teaser-2160p-1.mp4?rlkey=93s9yy5a7xghggjzfgyx113ub&st=zg4tmxi6&dl=0",
+  //   light: gallery_one,
+  // },
+  // {
+  //   url: "https://www.dropbox.com/scl/fi/al3ytsl8ag93mn0ntfz33/IMG_0520.PNG?rlkey=lmow2oxxyv7sp7wwad41ky4h6&st=tghnhjyw&dl=0",
+  //   light: gallery_two,
+  // },
+  // { url: "https://vimeo.com/990845187/f49e83fd83", light: gallery_five },
+  // {
+  //   url: "https://www.dropbox.com/scl/fi/qot2n92kio5msjwank9v5/IMG_0521.jpg?rlkey=1c23u59gb5nz8eqenxazhnwkj&st=k6dob4u6&dl=0",
+  //   light: gallery_three,
+  // },
+  // { url: "https://vimeo.com/990179377/a9a9266311", light: gallery_four },
 ];
 
 const Gallery = () => {
@@ -49,8 +50,44 @@ const Gallery = () => {
             Explore our latest stories in motion!
           </p>
         </div>
+      
 
-        <div id="works" className="flex flex-wrap justify-center gap-10">
+        <motion.div
+      id="works"
+      className="flex flex-wrap justify-center gap-10 p-5"
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
+    >
+      {videos.map((video, index) => (
+        <motion.div
+          key={index}
+          className="relative w-full sm:w-1/2 lg:w-1/3 xl:w-1/4 overflow-hidden rounded-lg shadow-lg bg-gray-900 p-3"
+          whileHover={{ scale: 1.05, rotate: 1 }}
+          transition={{ duration: 0.3 }}
+        >
+          <ReactPlayer
+            url={video.url}
+            width="100%"
+            height={400}
+            controls={true}
+            light={video.light}
+            playIcon={
+              <motion.button
+                className="bg-button border-2 border-button hover:bg-transparent hover:text-text duration-200 p-4 w-fit mx-auto font-bold md:px-12 md:py-6 text-white rounded-full shadow-md transition-all ease-in-out transform hover:scale-110 hover:rotate-3"
+                whileHover={{ scale: 1.15, rotate: -2 }}
+                whileTap={{ scale: 0.9 }}
+              >
+                PLAY VIDEO
+              </motion.button>
+            }
+          />
+        </motion.div>
+      ))}
+    </motion.div>
+
+
+        {/* <div id="works" className="flex flex-wrap justify-center gap-10">
           {videos.map((video, index) => (
             <div key={index} className="relative w-full md:w-1/2 lg:w-1/3">
               <ReactPlayer
@@ -68,7 +105,7 @@ const Gallery = () => {
               />
             </div>
           ))}
-        </div>
+        </div> */}
 
         <div className="text-center md:py-20 py-10">
           <h1 className="cursive md:text-6xl text-3xl md:my-6 my-3">
