affd372c65cf15005b133ce4c61aec3aa1449074
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-03-25 09:58:47 +0100

    rwrtwrt

--- src/components/bgChange.jsx
+++ src/components/bgChange.jsx
@@ -5,7 +5,7 @@ const BgChange = () => {
     <div className="relative w-full h-screen overflow-hidden">
       {/* Background Video */}
       <iframe
-        src="https://player.vimeo.com/video/1010058915?h=c7c983dfc5&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+        src="https://player.vimeo.com/video/1010058915?h=c7c983dfc5&autoplay=1&muted=1&background=1&badge=0&autopause=0&player_id=0&app_id=58479"
         className="absolute top-0 left-0 w-full h-full object-cover"
         frameBorder="0"
         allow="autoplay; fullscreen; picture-in-picture"
@@ -15,14 +15,6 @@ const BgChange = () => {
 
       {/* Overlay */}
       <div className="absolute inset-0 bg-black/40"></div>
-
-      {/* Text */}
-      {/* <div className="absolute inset-0 flex items-center justify-center text-center px-4">
-        <h1 className="text-white text-2xl md:text-4xl lg:text-5xl font-bold shadow-lg leading-snug max-w-2xl">
-          A wedding film is more than just a recording—it's a timeless keepsake, 
-          capturing the emotions and memories you'll cherish for a lifetime.
-        </h1>
-      </div> */}
     </div>
   );
 };
