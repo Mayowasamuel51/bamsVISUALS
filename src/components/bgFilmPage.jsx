3188ca3d4cef2ad56b498dfa7a26c1ba2dded1ef
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-03-25 10:03:21 +0100

    dfd

--- src/components/bgFilmPage.jsx
+++ src/components/bgFilmPage.jsx
@@ -1,12 +1,8 @@
-
-
-
-const BgFilmPage = ()=>{
-    return ( 
-        <>
-        <div className="relative w-full h-screen">
+const BgFilmPage = () => {
+  return (
+    <div className="relative w-full h-screen overflow-hidden">
       <iframe
-        src="https://player.vimeo.com/video/1021627312?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+        src="https://player.vimeo.com/video/1021627312?autoplay=1&muted=1&background=1&badge=0&autopause=0&player_id=0&app_id=58479"
         className="absolute top-0 left-0 w-full h-full"
         frameBorder="0"
         allow="autoplay; fullscreen; picture-in-picture"
@@ -14,10 +10,7 @@ const BgFilmPage = ()=>{
         title="Background Video"
       ></iframe>
     </div>
-        
-        
-        </>
-    )
-}
+  );
+};
 
 export default BgFilmPage;
