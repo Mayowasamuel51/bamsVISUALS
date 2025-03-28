ce75740ea7841b3ddb27663068b3a25ada727cb6
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-03-28 10:28:15 +0100

    difndsi

--- src/components/bgChange.jsx
+++ src/components/bgChange.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const BgChange = () => {
   return (
-    <div className="relative w-full h-screen overflow-hidden">
+    <div className="fixed top-0 left-0 w-full h-full overflow-hidden">
       {/* Background Video */}
       <iframe
         src="https://player.vimeo.com/video/1010058915?h=c7c983dfc5&autoplay=1&muted=1&background=1&badge=0&autopause=0&player_id=0&app_id=58479"
-        className="absolute top-0 left-0 w-full h-full object-cover"
+        className="absolute top-0 left-0 w-full h-full"
         frameBorder="0"
         allow="autoplay; fullscreen; picture-in-picture"
         allowFullScreen
